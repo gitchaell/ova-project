08cc7b9c82000ee1004d2a32c6a3a2f621285c46
f.michaell.a.m <f.michaell.a.m@gmail.com>
2024-10-13 22:40:50 +0000

    feat: add db tables

--- db/config.ts
+++ db/config.ts
@@ -23,10 +23,15 @@ const Session = defineTable({
 	},
 })
 
-const Ova = defineTable({
+const Course = defineTable({
 	columns: {
 		id: column.text({ primaryKey: true }),
 		title: column.text({ optional: false }),
+		level: column.text({ optional: false }),
+		concepts: column.text({ optional: false }),
+		start: column.date({ optional: false }),
+		end: column.date({ optional: false }),
+		schedules: column.text({ optional: false }),
 		userId: column.text({
 			optional: false,
 			references: () => User.columns.id,
@@ -34,7 +39,30 @@ const Ova = defineTable({
 	},
 })
 
+const Lesson = defineTable({
+	columns: {
+		id: column.text({ primaryKey: true }),
+		title: column.text({ optional: false }),
+		caption: column.text({ optional: false }),
+		start: column.text({ optional: false }),
+		end: column.text({ optional: false }),
+		done: column.boolean({ default: false }),
+		//
+		intro: column.text({ optional: true }),
+		before: column.text({ optional: true }),
+		during: column.text({ optional: true }),
+		after: column.text({ optional: true }),
+		quiz: column.text({ optional: true }),
+		urls: column.json({ optional: true }),
+		//
+		courseId: column.text({
+			optional: false,
+			references: () => Course.columns.id,
+		}),
+	},
+})
+
 // https://astro.build/db/config
 export default defineDb({
-	tables: { User, Session, Ova },
+	tables: { User, Session, Course, Lesson },
 })
