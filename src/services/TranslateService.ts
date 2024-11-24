2ec91f4ed3ff4e5e5875e99b8d37c6d04bd849d4
f.michaell.a.m <f.michaell.a.m@gmail.com>
2024-11-24 23:40:07 +0000

    fix: services

--- src/services/TranslateService.ts
+++ src/services/TranslateService.ts
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 
 export class TranslateService {
 	static async translate(text: string, sourceLang = 'auto', targetLang = 'en') {
-		const escapedStr = encodeURIComponent(text)
-		const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${escapedStr}`
-
 		try {
+			const escapedStr = encodeURIComponent(text)
+			const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${escapedStr}`
+
 			const response = await fetch(url)
 			const data = (await response.json()) as any[]
 			const result = data?.[0]?.[0]?.[0] || text
